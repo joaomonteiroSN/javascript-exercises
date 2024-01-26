c2d780c5b1dda1e3bc087f643b71803faf85aea8
joaomonteiroSN <70484799+joaomonteiroSN@users.noreply.github.com>
2024-01-26 17:07:44 -0300

    algoritmos e testes com jest, exercicios 1 e 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function (string, number) {
+    let counter = 0;
+    const result = [];
 
+    if (number < 0) {
+        return "ERROR";
+    } else {
+        while (counter < number) {
+            result.push(string);
+            counter++;
+        }
+    }
+
+    let resultStringed = result.join('');
+
+    return resultStringed;
 };
+
+// console.log(repeatString(phrase, 3))
 
 // Do not edit below this line
 module.exports = repeatString;
