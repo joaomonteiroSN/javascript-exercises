2a2f095fc57ea206e75095adea137e4486983c6f
joaomonteiroSN <70484799+joaomonteiroSN@users.noreply.github.com>
2024-01-29 13:40:39 -0300

    resolução até exercicio 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celcius = (((temp - 32) * (5/9)));
+  let rounded = Math.round(celcius * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fah = ((temp * 9/5) + 32);
+  let rounded = Math.round(fah * 10) / 10;
+  return rounded
 };
+
+console.log(convertToCelsius(-100));
 
 // Do not edit below this line
 module.exports = {
